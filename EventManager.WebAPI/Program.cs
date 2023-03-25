e4b94d41fc871d1cd2dcf0707471c97eb94c682e
Arseniy <dhhxdjsjdh@gmail.com>
2023-03-25 14:30:02 +0300

    add jwt bearer authorization in swagger

--- EventManager.WebAPI/Program.cs
+++ EventManager.WebAPI/Program.cs
@@ -4,13 +4,41 @@ using EventManager.Application.Common.Mappings;
 using EventManager.Application.Interfaces;
 using EventManager.Identity;
 using EventManager.Persistence;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme." +
+                      " \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below." +
+                      "\r\n\r\nExample: \"Bearer 12345abcdef\""
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 builder.Services.AddAutoMapper(config =>
 {
@@ -27,7 +55,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1"));
 }
 
 app.UseHttpsRedirection();
