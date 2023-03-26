7eaa5e8ce9203e0eb19ead661ded7885cc5a514d
Arseniy <dhhxdjsjdh@gmail.com>
2023-03-26 23:46:54 +0300

    add swagger in production environment

--- EventManager.WebAPI/Program.cs
+++ EventManager.WebAPI/Program.cs
@@ -57,12 +57,9 @@ builder.Services.AddSecurity(configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1"));
-}
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1"));
 
 app.UseHttpsRedirection();
 
